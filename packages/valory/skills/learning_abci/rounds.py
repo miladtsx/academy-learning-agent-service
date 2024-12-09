e4c0ccd517134745a46222afd0e54ce56451efdc
MehdizadeMilad <milad.mehdizade1394@gmail.com>
2024-12-09 12:16:48 +0000

    feat(dcpay): define rounds

--- packages/valory/skills/learning_abci/rounds.py
+++ packages/valory/skills/learning_abci/rounds.py
@@ -19,25 +19,29 @@
 
 """This package contains the rounds of LearningAbciApp."""
 
+from abc import ABC
 from enum import Enum
-from typing import Dict, FrozenSet, Optional, Set, Tuple
-
+from typing import Dict, FrozenSet, Mapping, Optional, Set, Tuple, Type, cast
 from packages.valory.skills.abstract_round_abci.base import (
     AbciApp,
     AbciAppTransitionFunction,
+    AbstractRound,
     AppState,
     BaseSynchronizedData,
+    CollectDifferentUntilAllRound,
     CollectSameUntilThresholdRound,
     CollectionRound,
     DegenerateRound,
     DeserializedCollection,
     EventToTimeout,
+    OnlyKeeperSendsRound,
     get_name,
 )
 from packages.valory.skills.learning_abci.payloads import (
-    DataPullPayload,
+    InvoicesPayload,
     DecisionMakingPayload,
-    TxPreparationPayload,
+    KeeperPayload,
+    ConfirmationResultPayload,
 )
 
 
@@ -46,7 +50,7 @@ class Event(Enum):
 
     DONE = "done"
     ERROR = "error"
-    TRANSACT = "transact"
+    NO_INVOICE = "no_invoice"
     NO_MAJORITY = "no_majority"
     ROUND_TIMEOUT = "round_timeout"
 
@@ -62,6 +66,78 @@ class SynchronizedData(BaseSynchronizedData):
         """Strictly get a collection and return it deserialized."""
         serialized = self.db.get_strict(key)
         return CollectionRound.deserialize_collection(serialized)
+
+    # DATA PULL ROUND
+    @property
+    def participant_to_invoices(self) -> Mapping[str, InvoicesPayload]:
+        """Get the participant_to_invoices."""
+        return cast(
+            Mapping[str, InvoicesPayload],
+            self._get_deserialized("participant_to_invoices"),
+        )
+
+    # @property
+    # def collected_invoices(self) -> List[str]:
+    #     """Get the invoices."""
+    #     return (
+    #         value.invoices for value in self.participant_to_invoices.values()
+    #     )
+
+    # ON-CHAIN MONITORING ROUND
+    @property
+    def agent_on_chain_monitoring_round(self) -> Optional[set[str]]:
+        """List of onchain monitoring gathered by agents"""
+        return self.db.get("agent_on_chain_monitoring_round")
+
+    @property
+    def invoices(self) -> Optional[str]:
+        """Get the list of Invoices"""
+        return self.db.get("invoices", None)
+
+    @property
+    def agent_to_invoices_round(self) -> DeserializedCollection:
+        """
+        Agent to payload mapping for
+            the CollectInvoicesRound, passing the invoices received from the business API to the DecisionMakingRound
+        """
+        return self._get_deserialized("agent_to_invoices_round")
+
+    @property
+    def participant_to_webhook_round(self) -> DeserializedCollection:
+        """
+        Agent to payload mapping for the DecisionMakingRound;
+        passing the uuid of the invoices that are settled and must be updated via the Webhook.
+        """
+        return self._get_deserialized("participant_to_webhook_round")
+
+    @property
+    def settled_invoices_uuid(self) -> Optional[str]:
+        """Get the list of settled invoices"""
+        return self.db.get("settled_invoices_uuid", None)
+
+    @property
+    def participant_to_select_keeper_round(self) -> DeserializedCollection:
+        """
+        Selected Keeper at the SelectKeeperBehaviour;
+        """
+        return self._get_deserialized("participant_to_select_keeper_round")
+
+    @property
+    def selected_keeper(self) -> Optional[str]:
+        """Get the selected keeper address"""
+        return self.db.get("selected_keeper", None)
+
+    @property
+    def participant_to_webhook_ok(self) -> DeserializedCollection:
+        """
+        Selected Keeper at the ConfirmationRound;
+        """
+        return self._get_deserialized("participant_to_webhook_ok")
+
+    @property
+    def is_webhook_ok(self) -> Optional[bool]:
+        """Get the webhook status"""
+        return self.db.get("is_webhook_ok", None)
 
     @property
     def price(self) -> Optional[float]:
@@ -84,11 +160,6 @@ class SynchronizedData(BaseSynchronizedData):
         return self.db.get("erc20_balance", None)
 
     @property
-    def participant_to_data_round(self) -> DeserializedCollection:
-        """Agent to payload mapping for the DataPullRound."""
-        return self._get_deserialized("participant_to_data_round")
-
-    @property
     def most_voted_tx_hash(self) -> Optional[float]:
         """Get the token most_voted_tx_hash."""
         return self.db.get("most_voted_tx_hash", None)
@@ -104,117 +175,131 @@ class SynchronizedData(BaseSynchronizedData):
         return str(self.db.get_strict("tx_submitter"))
 
 
-class DataPullRound(CollectSameUntilThresholdRound):
-    """DataPullRound"""
+class DPAYABCIAbstractRound(AbstractRound, ABC):
+    """Abstract round for the DCPAY ABCI skill."""
 
-    payload_class = DataPullPayload
+    synchronized_data_class: Type[BaseSynchronizedData] = SynchronizedData
+
+    @property
+    def synchronized_data(self) -> SynchronizedData:
+        """Return the synchronized data."""
+        return cast(SynchronizedData, self._synchronized_data)
+
+
+class CollectInvoicesRound(CollectSameUntilThresholdRound, DPAYABCIAbstractRound):
+    """CollectInvoicesRound"""
+
+    payload_class = InvoicesPayload
     synchronized_data_class = SynchronizedData
     done_event = Event.DONE
     no_majority_event = Event.NO_MAJORITY
 
-    # Collection key specifies where in the synchronized data the agento to payload mapping will be stored
-    collection_key = get_name(SynchronizedData.participant_to_data_round)
-
-    # Selection key specifies how to extract all the different objects from each agent's payload
-    # and where to store it in the synchronized data. Notice that the order follows the same order
-    # from the payload class.
-    selection_key = (
-        get_name(SynchronizedData.price),
-        get_name(SynchronizedData.price_ipfs_hash),
-        get_name(SynchronizedData.native_balance),
-        get_name(SynchronizedData.erc20_balance),
-    )
-
-    # Event.ROUND_TIMEOUT  # this needs to be referenced for static checkers
+    collection_key = get_name(SynchronizedData.participant_to_invoices)
+    selection_key = (get_name(SynchronizedData.invoices))
 
 
-class DecisionMakingRound(CollectSameUntilThresholdRound):
-    """DecisionMakingRound"""
+class DecisionMakingRound(CollectSameUntilThresholdRound, DPAYABCIAbstractRound):
+    """
+    DecisionMakingRound
+
+    Monitor onchain events to see if an invoice is settled.
+    """
 
     payload_class = DecisionMakingPayload
     synchronized_data_class = SynchronizedData
-
-    # Since we need to execute some actions after consensus, we override the end_block method
-    # instead of just setting the selection and collection keys
-    def end_block(self) -> Optional[Tuple[BaseSynchronizedData, Event]]:
-        """Process the end of the block."""
-
-        if self.threshold_reached:
-            event = Event(self.most_voted_payload)
-            return self.synchronized_data, event
-
-        if not self.is_majority_possible(
-            self.collection, self.synchronized_data.nb_participants
-        ):
-            return self.synchronized_data, Event.NO_MAJORITY
-
-        return None
-
-    # Event.DONE, Event.ERROR, Event.TRANSACT, Event.ROUND_TIMEOUT  # this needs to be referenced for static checkers
+    done_event = Event.DONE
+    no_majority_event = Event.NO_MAJORITY
+    collection_key = get_name(SynchronizedData.participant_to_webhook_round)
+    selection_key = (get_name(SynchronizedData.settled_invoices_uuid))
 
 
-class TxPreparationRound(CollectSameUntilThresholdRound):
-    """TxPreparationRound"""
+class SelectKeeperRound(CollectSameUntilThresholdRound, DPAYABCIAbstractRound):
+    """
+    SelectKeeperRound
+    Select the agent to invoke the webhook.
+    """
 
-    payload_class = TxPreparationPayload
+    payload_class = KeeperPayload
     synchronized_data_class = SynchronizedData
     done_event = Event.DONE
     no_majority_event = Event.NO_MAJORITY
-    collection_key = get_name(SynchronizedData.participant_to_tx_round)
-    selection_key = (
-        get_name(SynchronizedData.tx_submitter),
-        get_name(SynchronizedData.most_voted_tx_hash),
-    )
 
-    # Event.ROUND_TIMEOUT  # this needs to be referenced for static checkers
+    collection_key = get_name(SynchronizedData.participant_to_select_keeper_round)
+    selection_key = get_name(SynchronizedData.selected_keeper)
 
 
-class FinishedDecisionMakingRound(DegenerateRound):
-    """FinishedDecisionMakingRound"""
+class ConfirmationRound(CollectDifferentUntilAllRound, DPAYABCIAbstractRound):
+    """
+    Confirm the invoice settlement via the webhook so the business can continue the purchase flow.
+    """
+
+    keeper_payload: Optional[ConfirmationResultPayload] = None
+    payload_class = ConfirmationResultPayload
+    synchronized_data_class = SynchronizedData
+    done_event = Event.DONE
+
+    collection_key = get_name(SynchronizedData.participant_to_webhook_ok)
+    selection_key = get_name(SynchronizedData.is_webhook_ok)
+
+    def end_block(self) -> Optional[Tuple[BaseSynchronizedData, Event]]:
+        """Process the end of the block."""
+
+        if self.collection_threshold_reached:
+            synchronized_data = self.synchronized_data.update(
+                participants=tuple(sorted(self.collection)),
+                synchronized_data_class=SynchronizedData,
+            )
+            return synchronized_data, Event.DONE
+        return None
 
 
-class FinishedTxPreparationRound(DegenerateRound):
-    """FinishedLearningRound"""
+class FinishedRound(DegenerateRound):
+    """FinishedRound"""
 
 
 class LearningAbciApp(AbciApp[Event]):
     """LearningAbciApp"""
 
-    initial_round_cls: AppState = DataPullRound
+    initial_round_cls: AppState = CollectInvoicesRound
     initial_states: Set[AppState] = {
-        DataPullRound,
+        CollectInvoicesRound,
     }
     transition_function: AbciAppTransitionFunction = {
-        DataPullRound: {
-            Event.NO_MAJORITY: DataPullRound,
-            Event.ROUND_TIMEOUT: DataPullRound,
+        CollectInvoicesRound: {
+            Event.NO_MAJORITY: CollectInvoicesRound,
+            Event.ROUND_TIMEOUT: CollectInvoicesRound,
+            Event.NO_INVOICE: CollectInvoicesRound,
             Event.DONE: DecisionMakingRound,
         },
         DecisionMakingRound: {
-            Event.NO_MAJORITY: DecisionMakingRound,
+            Event.NO_MAJORITY: CollectInvoicesRound,
             Event.ROUND_TIMEOUT: DecisionMakingRound,
-            Event.DONE: FinishedDecisionMakingRound,
-            Event.ERROR: FinishedDecisionMakingRound,
-            Event.TRANSACT: TxPreparationRound,
+            Event.DONE: SelectKeeperRound,
+            Event.ERROR: CollectInvoicesRound,
+            Event.NO_INVOICE: CollectInvoicesRound,
         },
-        TxPreparationRound: {
-            Event.NO_MAJORITY: TxPreparationRound,
-            Event.ROUND_TIMEOUT: TxPreparationRound,
-            Event.DONE: FinishedTxPreparationRound,
+        SelectKeeperRound: {
+            Event.NO_MAJORITY: SelectKeeperRound,
+            Event.ROUND_TIMEOUT: SelectKeeperRound,
+            Event.DONE: ConfirmationRound,
+            Event.ERROR: SelectKeeperRound,
+            Event.NO_INVOICE: CollectInvoicesRound,
         },
-        FinishedDecisionMakingRound: {},
-        FinishedTxPreparationRound: {},
+        ConfirmationRound: {
+            Event.NO_MAJORITY: ConfirmationRound,
+            Event.ROUND_TIMEOUT: ConfirmationRound,
+            Event.DONE: FinishedRound,
+        },
+        FinishedRound: {},
     }
     final_states: Set[AppState] = {
-        FinishedDecisionMakingRound,
-        FinishedTxPreparationRound,
+        FinishedRound,
     }
     event_to_timeout: EventToTimeout = {}
     cross_period_persisted_keys: FrozenSet[str] = frozenset()
     db_pre_conditions: Dict[AppState, Set[str]] = {
-        DataPullRound: set(),
+        CollectInvoicesRound: set(),
     }
     db_post_conditions: Dict[AppState, Set[str]] = {
-        FinishedDecisionMakingRound: set(),
-        FinishedTxPreparationRound: {get_name(SynchronizedData.most_voted_tx_hash)},
+        FinishedRound: set(),
     }
